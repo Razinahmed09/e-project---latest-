b9553f655360d1abb7e4e54b14789aa15ff9869a
Razin Ahmed <raheenahmee@gmail.com>
2024-05-27 06:58:16 -0700

    hello

--- js/index.js
+++ js/index.js
@@ -1,44 +1,22 @@
-$( document ).ready(function() {
-    
-    $('.slider22').slick({
-        dots: false,
-        infinite: true,
-        speed: 150,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows:false,
-        autoplay: true,
-        dots:false,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
 
-});
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "3",
+    coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: true,
+    loop: true,
 
-
+  },
+  autoplay: {
+    delay: 2000,
+  },
+    pagination: {
+    el: ".swiper-pagination",
+    },
+  });
